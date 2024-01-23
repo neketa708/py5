5fc93801f8603b019ce14c21d0bf14d1280864d4
NEKETA <molnei1996@gmail.com>
2024-01-23 23:53:44 +0300

    add_zad

---
+++ zad1.py
@@ -0,0 +1,11 @@
+"""
+Напишите функцию, которая принимает на вход строку - абсолютный путь до файла. Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+"""
+def get_file_info(file_path):
+    file_name = file_path.split("/")[-1]
+    file_extension = file_name.split(".")[-1]
+    path = file_path[:-len(file_name)]
+    return (path, file_name[:-len(file_extension)-1], "." + file_extension)
+
+file_path = "C:/Users/User/Documents/example.txt"
+print(get_file_info(file_path))
